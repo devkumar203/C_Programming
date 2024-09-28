31b152916a10ff9a2ed4e89d8070a15a650ef012
DEV KUMAR <devkumar20003@gmail.com>
2024-09-29 00:43:52 +0530

    Just Commit

--- Gross_Salary.c
+++ Gross_Salary.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    /*
+    /*https://github.com/devkumar203/C_Programming
     Ramesh's basic salary is input through the keyboard. His dearness 
     allowance is 40% of basic salary, and house rent allowance is 20% of
     basic salry. Write a program to calculate his gross salary.
